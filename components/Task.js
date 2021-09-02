0813e1ef42f39631ef9ff280a798978cee059d16
NicolleInterior <intercoeli@gmail.com>
2021-09-02 17:17:52 +0800

    layout the task.js for the style of each task

--- components/Task.js
+++ components/Task.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Task = (props) => {
     return (
-        <View>
-            <Text>{props.text}</Text>
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View style={styles.circular}></View>
         </View>
     )
 
