486374d207e32f9c5a3d2f8787c75c43c7ae5f94
NicolleInterior <intercoeli@gmail.com>
2021-09-02 17:12:01 +0800

    export Task.js and imported it in App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -11,7 +12,12 @@ export default function App() {
 
           <View style={styles.items}>
             {/* This is where the tasks will go */}
-
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />    
           </View>
       </View>
     </View>
---
+++ components/Task.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = () => {
+    return (
+        <View>
+           <Text>This is a task</Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+
+
+export default Task;
